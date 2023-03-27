ff6265b6d5380fdc3159a93560cd4c4a1375cb72
BenAllington95 <103380285+BenAllington95@users.noreply.github.com>
2023-03-27 14:46:40 +0100

    Created function that will replace quotes from api

--- quizzical/src/components/Question.jsx
+++ quizzical/src/components/Question.jsx
@@ -17,28 +17,46 @@ export default function Question (props) {
         )
     })
 
-    function handleQuestion(str) {
+    // function handleQuestion(str) {
 
-        const replacements = {
-            '&#039;': "'",
-            '&quot;' : "'"
-          };
+    //     const replacements = {
+    //         '&#039;': "'",
+    //         '&quot;' : "'"
+    //       };
 
-        const regex = new RegExp(Object.keys(replacements).join(' '), 'ig')
+    //     const regex = new RegExp(Object.keys(replacements).join(' '), 'ig')
 
-        const newString = str.replace(regex, (match) => {
-            // use the matched substring as a key to find the corresponding replacement string
-            return replacements[match.toLowerCase()];
-          });
+    //     const newString = str.replace(regex, (match) => {
+    //         // use the matched substring as a key to find the corresponding replacement string
+    //         return replacements[match.toLowerCase()];
+    //       });
 
-        return newString    
-    }
+    //     return newString    
+    // } 
+
+    function handleQuestionString() {
+        const string = props.question
+        console.log(string)
+
+        const replaceQuestionStr = string.replace(/&#039;|&quot;/g, (match) => {
+            if (match === "&#039;") { // &#039; == '
+                return "'";
+              } else if (match === "&quot;") { // &quot; == "
+                return `"`;
+              } else {
+                return match;
+              }
+            })
+
+        return replaceQuestionStr
+    } // replaces two arguments that need to be changed when being called from api, this includes the double quote and apostrophe symbols
+
 
 
 
     return (
         <div className="question">
-            <h3>{handleQuestion(props.question)}</h3>
+            <h3>{handleQuestionString()}</h3>
             <div className="answers">
                 {answerElements}
             </div>
